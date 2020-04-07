3c125313cd28700f11b26a834a347d288fb899db
roma0398 <roma0398@mail.ru>
2020-04-08 02:50:14 +0300

    add zoom v1

--- MyApplication3/app/src/main/java/com/example/myapplication3/voz/common_advice/VozCommon1.java
+++ MyApplication3/app/src/main/java/com/example/myapplication3/voz/common_advice/VozCommon1.java
@@ -1,18 +1,25 @@
 package com.example.myapplication3.voz.common_advice;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication3.R;
 
 public class VozCommon1 extends AppCompatActivity {
-
+    private ScaleGestureDetector mScaleGestureDetector;
+    private float mScaleFactor = 1.0f;
+    private ImageView mImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voz_common1);
+        mImageView=(ImageView)findViewById(R.id.imageView);
+        mScaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
         this.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -20,5 +27,21 @@ public class VozCommon1 extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        mScaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
+    }
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        mScaleGestureDetector.onTouchEvent(motionEvent);
+        return true;
+    }
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector){
+            mScaleFactor *= scaleGestureDetector.getScaleFactor();
+            mScaleFactor = Math.max(0.1f,
+                    Math.min(mScaleFactor, 10.0f));
+            mImageView.setScaleX(mScaleFactor);
+            mImageView.setScaleY(mScaleFactor);
+            return true;
+        }
     }
 }
--- MyApplication3/app/src/main/java/com/example/myapplication3/voz/common_advice/VozCommon2.java
+++ MyApplication3/app/src/main/java/com/example/myapplication3/voz/common_advice/VozCommon2.java
@@ -1,18 +1,27 @@
 package com.example.myapplication3.voz.common_advice;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication3.R;
+import com.github.chrisbanes.photoview.PhotoView;
+import com.github.chrisbanes.photoview.PhotoViewAttacher;
 
 public class VozCommon2 extends AppCompatActivity {
 
+    ImageView imageView ;
+
+    PhotoViewAttacher photoViewAttacher ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voz_common2);
+        PhotoView photoView = (PhotoView) findViewById(R.id.imageView);
+        photoView.setImageResource(R.drawable.voz_common2);
         this.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
