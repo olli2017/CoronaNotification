6773afa1f7d63a6924761884fc83382284839916
Aydar <dusmukhambetov@mail.ru>
2020-04-08 01:42:30 +0300

    info in infection + refactor. just show image

---
+++ MyApplication3/app/src/main/java/com/example/myapplication3/concreteAdvice/DisplayInfectionAdviceActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication3.concreteAdvice;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication3.R;
+
+public class DisplayInfectionAdviceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_infection_advice);
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+}
