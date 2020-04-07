843a49d1f620976131adee7223230f4cb9328b37
roma0398 <roma0398@mail.ru>
2020-04-07 14:00:09 +0300

    hide navigation bar

--- MyApplication3/app/src/main/java/com/example/myapplication3/Voz1.java
+++ MyApplication3/app/src/main/java/com/example/myapplication3/Voz1.java
@@ -3,6 +3,7 @@ package com.example.myapplication3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Voz1 extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class Voz1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voz1);
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);    
     }
 }
