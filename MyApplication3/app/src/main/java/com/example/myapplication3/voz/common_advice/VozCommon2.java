8ad3b58edbceeae88301ea513b7d8cdb563cb7d0
roma0398 <roma0398@mail.ru>
2020-04-08 03:10:35 +0300

    add zoom on all pictures in info

--- MyApplication3/app/src/main/java/com/example/myapplication3/voz/common_advice/VozCommon2.java
+++ MyApplication3/app/src/main/java/com/example/myapplication3/voz/common_advice/VozCommon2.java
@@ -1,20 +1,12 @@
 package com.example.myapplication3.voz.common_advice;
 
 import android.os.Bundle;
-import android.widget.ImageView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.myapplication3.MyUtils;
 import com.example.myapplication3.R;
 import com.github.chrisbanes.photoview.PhotoView;
-import com.github.chrisbanes.photoview.PhotoViewAttacher;
 
 public class VozCommon2 extends AppCompatActivity {
-
-    ImageView imageView ;
-
-    PhotoViewAttacher photoViewAttacher ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
