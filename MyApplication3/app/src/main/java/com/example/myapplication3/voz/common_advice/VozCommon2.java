8aa33292712bf26570e4661cf55117024fb1fed8
Aydar <dusmukhambetov@mail.ru>
2020-04-08 03:02:53 +0300

    extract method

--- MyApplication3/app/src/main/java/com/example/myapplication3/voz/common_advice/VozCommon2.java
+++ MyApplication3/app/src/main/java/com/example/myapplication3/voz/common_advice/VozCommon2.java
@@ -1,12 +1,11 @@
 package com.example.myapplication3.voz.common_advice;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.myapplication3.MyUtils;
 import com.example.myapplication3.R;
 import com.github.chrisbanes.photoview.PhotoView;
 import com.github.chrisbanes.photoview.PhotoViewAttacher;
@@ -22,12 +21,6 @@ public class VozCommon2 extends AppCompatActivity {
         setContentView(R.layout.activity_voz_common2);
         PhotoView photoView = (PhotoView) findViewById(R.id.imageView);
         photoView.setImageResource(R.drawable.voz_common2);
-        this.getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        MyUtils.setsetSystemUiVisibility(this);
     }
 }
